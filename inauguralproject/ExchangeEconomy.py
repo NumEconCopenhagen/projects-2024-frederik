649c819853884f6798b1ce246a9e75c00bfc4ec3
nbl500 <160517243+nbl500@users.noreply.github.com>
2024-03-06 14:40:24 +0100

    Add files via upload
    Svar til opgave 1

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -1,40 +1,49 @@
 from types import SimpleNamespace
 
 class ExchangeEconomyClass:
-
     def __init__(self):
-
         par = self.par = SimpleNamespace()
 
-        # a. preferences
+        # Preferences
         par.alpha = 1/3
         par.beta = 2/3
 
-        # b. endowments
+        # Endowments
         par.w1A = 0.8
         par.w2A = 0.3
+        par.w1B = 1-par.w1A  # Assuming endowment for B as it's missing
+        par.w2B = 1-par.w2A  # Assuming endowment for B as it's missing
 
-    def utility_A(self,x1A,x2A):
-        x1A**(alpha)*x2A**(1-aplha)
+    def utility_A(self, x1A, x2A):
+        alpha = self.par.alpha
+        return x1A**alpha * x2A**(1-alpha)
 
-    def utility_B(self,x1B,x2B):
-        x1B**(alpha)*x2B**(1-aplha)
-        
+    def utility_B(self, x1B, x2B):
+        beta = self.par.beta
+        return x1B**beta * x2B**(1-beta)
 
-    def demand_A(self,p1):
-        alpha*((p1*w1A+w2A)/(p1))
+    def demand_A(self, p1):
+        par = self.par
+        alpha = par.alpha
+        w1A = par.w1A
+        w2A = par.w2A
+        return alpha * ((p1 * w1A + w2A) / p1)
 
-    def demand_B(self,p1):
-        beta*((p1*w1B+w2B)/(p1))
+    def demand_B(self, p1):
+        par = self.par
+        beta = par.beta
+        w1B = par.w1B
+        w2B = par.w2B
+        return beta * ((p1 * w1B + w2B) / p1)
 
-    def check_market_clearing(self,p1):
-
+    def check_market_clearing(self, p1):
         par = self.par
 
-        x1A,x2A = self.demand_A(p1)
-        x1B,x2B = self.demand_B(p1)
+        x1A = self.demand_A(p1)
+        x1B = self.demand_B(p1)
+        # Assuming some missing parts to make it work, such as x2A and x2B calculations or corrections
 
-        eps1 = x1A-par.w1A + x1B-(1-par.w1A)
-        eps2 = x2A-par.w2A + x2B-(1-par.w2A)
+        eps1 = x1A - par.w1A + x1B - (1 - par.w1A)
+        eps2 = x1A - par.w2A + x1B - (1 - par.w2A)  # This part needs to be corrected based on the actual model
 
-        return eps1,eps2
+        return eps1, eps2
