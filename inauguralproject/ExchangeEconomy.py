5079747c59365fe2777d3d000aab573360eae7aa
nbl500 <160517243+nbl500@users.noreply.github.com>
2024-03-06 13:07:49 +0100

    Add files via upload

---
+++ inauguralproject/ExchangeEconomy.py
@@ -0,0 +1,40 @@
+from types import SimpleNamespace
+
+class ExchangeEconomyClass:
+
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        # a. preferences
+        par.alpha = 1/3
+        par.beta = 2/3
+
+        # b. endowments
+        par.w1A = 0.8
+        par.w2A = 0.3
+
+    def utility_A(self,x1A,x2A):
+        x1A**(alpha)*x2A**(1-aplha)
+
+    def utility_B(self,x1B,x2B):
+        x1B**(alpha)*x2B**(1-aplha)
+        
+
+    def demand_A(self,p1):
+        alpha*((p1*w1A+w2A)/(p1))
+
+    def demand_B(self,p1):
+        beta*((p1*w1B+w2B)/(p1))
+
+    def check_market_clearing(self,p1):
+
+        par = self.par
+
+        x1A,x2A = self.demand_A(p1)
+        x1B,x2B = self.demand_B(p1)
+
+        eps1 = x1A-par.w1A + x1B-(1-par.w1A)
+        eps2 = x2A-par.w2A + x2B-(1-par.w2A)
+
+        return eps1,eps2
